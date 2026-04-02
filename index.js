f767bdda4ca5a9ed0c18abcaf067966e73f0c31b
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-04-02 02:07:04 +0200

    First commit, need work to finish

---
+++ index.js
@@ -0,0 +1,57 @@
+const changeMode = document.querySelector('#circle-icon');
+const baseMode = document.querySelector('#box-light');
+const bodyElement = document.querySelector('body');
+const mainBody = document.querySelector('#main');
+const footerElement = document.querySelector('#copy');
+const headerElement = document.querySelector('#head-list');
+const newSection = document.querySelector('#list-container');
+const addElement = document.querySelector('#submit-list');
+const reasonList = document.querySelector('#to-do');
+
+
+
+changeMode.addEventListener('click', () => {
+    bodyElement.style.backgroundColor = "black";
+    mainBody.style.backgroundColor = 'rgb(48, 48, 46)';
+    footerElement.style.backgroundColor = 'rgb(32, 71, 81)';
+    footerElement.style.color = 'white';
+    headerElement.style.backgroundColor = 'rgb(32, 71, 81)';
+    baseMode.style.justifyContent = 'right';
+
+
+    bodyElement.classList.add('light-mode');
+    bodyElement.classList.toggle('light-mode');
+
+    footerElement.classList.toggle('color');
+})
+
+addElement.addEventListener('click', () => {
+    const newList = document.createElement('section');
+    newList.innerHTML = `
+        <div id="list">
+            <h2 id="task-name">${reasonList.value}</h2>
+                <div id="list-btn">
+                    <button type="submit" id="task-done">Done</button>
+                    <button type="reset" id="task-delete">Delete</button>
+                </div>
+        </div>
+    `;
+
+
+    newSection.appendChild(newList);
+
+    const doneTask = document.querySelector('#task-done');
+
+    doneTask.addEventListener('click', () => {
+        doneTask.classList.toggle('color');
+    });
+
+    const deleteTask = document.querySelector('#task-delete');
+
+    deleteTask.addEventListener('click', () =>{
+        newList.remove();
+    })
+    
+});
+
+
---
+++ moon.js
@@ -0,0 +1,64 @@
+"use strict"
+
+const canvas = document.querySelector('#canvas-dark');
+
+const context = canvas.getContext("2d");
+
+
+
+
+context.fillStyle = "rgb(60, 58, 48)";
+context.strokeStyle = "rgb(81, 78, 66)";
+
+context.beginPath();
+context.arc(10.5,10, 3, 0, Math.PI * 2);
+context.fill();
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.rect(10, 2, 1, 3);
+context.stroke();
+context.fill();
+context.closePath();
+
+context.beginPath();
+context.rect(10, 16, 1, 3);
+context.stroke();
+context.fill();
+context.closePath();
+
+context.beginPath();
+context.rect(2, 10, 3, 1);
+context.stroke();
+context.fill();
+context.closePath();
+
+context.beginPath();
+context.rect(16, 10, 2, 1);
+context.stroke();
+context.fill();
+context.closePath();
+
+context.beginPath();
+context.moveTo(15, 6);
+context.lineTo(16, 3);
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.moveTo(15, 14);
+context.lineTo(17, 17);
+context.stroke();
+
+context.beginPath();
+context.moveTo(6,6);
+context.lineTo(3,4);
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.moveTo(6,16);
+context.lineTo(3,17);
+context.stroke();
+context.closePath();
